9d7a9cfab16db373f887ccf190b2b5b0eb62fe73
rexou23 <rexou@sina.com>
2019-03-10 23:43:15 +0800

    test2

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,13 @@
 public class Test1 {
     public void sayHi(){
+        //<editor-fold desc="折叠测试">
         System.out.println("Test1");
+        System.out.println("Test1");
+        System.out.println("Test1");
+        System.out.println("Test1");
+        System.out.println("Test1");
+        System.out.println("Test1");
+        System.out.println("Test1");
+        //</editor-fold>
     }
 }
